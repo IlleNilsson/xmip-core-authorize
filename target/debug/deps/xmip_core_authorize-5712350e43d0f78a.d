d8ea13ae29583cde8e1bf50ac746f9564f77fa09
Ilian Nilsson <ilian.nilsson@live.se>
2026-08-27 09:17:40 +0200

    Name the crate after its repository; take what this module owns

---
+++ target/debug/deps/xmip_core_authorize-5712350e43d0f78a.d
@@ -0,0 +1,5 @@
+D:\Repos\Xmip\modules\capabilities\authorize\target\debug\deps\xmip_core_authorize-5712350e43d0f78a.d: src\lib.rs
+
+D:\Repos\Xmip\modules\capabilities\authorize\target\debug\deps\xmip_core_authorize-5712350e43d0f78a.exe: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/xmip_core_party-bd3a4d6df0076254.d
@@ -0,0 +1,7 @@
+D:\Repos\Xmip\modules\capabilities\authorize\target\debug\deps\xmip_core_party-bd3a4d6df0076254.d: C:\Users\IlianNilsson\.cargo\git\checkouts\xmip-core-party-663f16e6a01f1a0c\1adb86b\src\lib.rs
+
+D:\Repos\Xmip\modules\capabilities\authorize\target\debug\deps\libxmip_core_party-bd3a4d6df0076254.rlib: C:\Users\IlianNilsson\.cargo\git\checkouts\xmip-core-party-663f16e6a01f1a0c\1adb86b\src\lib.rs
+
+D:\Repos\Xmip\modules\capabilities\authorize\target\debug\deps\libxmip_core_party-bd3a4d6df0076254.rmeta: C:\Users\IlianNilsson\.cargo\git\checkouts\xmip-core-party-663f16e6a01f1a0c\1adb86b\src\lib.rs
+
+C:\Users\IlianNilsson\.cargo\git\checkouts\xmip-core-party-663f16e6a01f1a0c\1adb86b\src\lib.rs:
